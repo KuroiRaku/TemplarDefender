55c5d3124f1b9fb6ba9cb514666ac0156c47e6b0
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-08-23 18:07:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TemplarDefender
+
+Developed with Unreal Engine 4
---
+++ Source/TemplarDefender/TemplarDefenderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TemplarDefenderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATemplarDefenderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATemplarDefenderGameMode();
+};
+
+
+
